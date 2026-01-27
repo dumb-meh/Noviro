bea22fe36080f04ecfc686134f46b44332559863
dumb-meh <omorfaruk549@gmail.com>
2026-01-27 17:08:40 +0600

    knowledge schema added

---
+++ app/utils/knowledge/knowledge_schema.py
@@ -0,0 +1,12 @@
+from typing import List
+
+Product_Name:str 
+Product_Price:float
+Product_Description:str
+Product_Category:str
+Product_Sub_Category:str
+Type:str
+Stock_Quantity:int
+Discount:float
+Tags:List[str]
+About_this_product:str
